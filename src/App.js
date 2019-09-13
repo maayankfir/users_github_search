10db8419521248974ed0c172a22ef27d9000ea4c
Maayan Kfir <mayankfir@gmail.com>
2019-09-12 21:13:03 -0400

    message

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Navbar from './layout/Navbar';
+import Users from './components/users/Users';
+import Search from './components/users/Search';
+import './App.css';
+
+import axios from 'axios';
+
+class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  searchUsers = async text => {
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=
+    ${process.env.REACT_APP_GITHUB_CLIENT_ID}
+    &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+        <div className='container'>
+          <Search searchUsers={this.searchUsers} />
+          <Users users={this.state.users} loading={this.state.loading} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
